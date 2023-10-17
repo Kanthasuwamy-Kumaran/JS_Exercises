071a2b8c2377c8a65cb834443c2d44db281f8996
Kanthasuwamy-Kumaran <rkkumaran834@gmail.com>
2023-10-17 15:52:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,161 @@
+//01 
+//comments can make code readable
+
+//02 
+//Welcome to learn Javascript
+
+//03 
+/*code readable, 
+easy to reuse and informative*/
+
+//04
+// var a = String;
+let A="Kumaran";
+let B=true;
+let C;
+let D=null;
+
+//05
+console.log(typeof A);
+console.log(typeof B);
+console.log(typeof C);
+console.log(typeof D);
+
+//06
+var Name1;
+var Name2;
+var Name3;
+var Name4;
+
+//07
+var Name1=5;
+var Name2=4;
+var Name3=3;
+var Name4=2;
+
+//08
+// var
+
+//09
+var first_name,last_name,arital_status,country,age;
+
+
+//Day2  JS Exercises
+
+//1
+var Name = "Learn JavaScript";
+
+//2
+console.log(Name);
+
+//3
+console.log(Name.length);
+
+//4
+console.log(Name.toUpperCase());
+
+//5
+console.log(Name.toLowerCase());
+
+//6
+console.log(Name.substring(15));
+
+//7
+let learn = "Welcome to the JavaScript";
+console.log(learn.length);
+console.log(learn.substring(19));
+
+//8
+console.log(learn.includes("Welcome"))  /* identify */
+
+//9
+var split= 'learn JavaScript';
+var split1 = split.split("");
+console.log(split1);
+
+//10
+var split2= 'learn JavaScript';
+var split3 = split2.split(" ");
+console.log(split3);
+
+11
+let media = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+let newMedia = media.split(',');
+console.log(newMedia);
+
+12
+let word = "learn Javascript";
+console.log(word);
+console.log(word.replace("learn Javascript","30 Days Of Python"));
+
+//13
+let  Learn = 'learn Javascript'
+console.log(Learn.length);
+console.log( Learn.charAt(15)) ;
+
+//14
+let code = 'learn Javascript'
+console.log(code.charCodeAt(3)); //Don't no
+
+//15
+var position = 'learn Javascript';
+console.log(position.indexOf('a'));
+
+//16
+var determine = 'learn Javascript';
+console.log(determine.lastIndexOf('a'));
+
+//17
+var first = "You cannot end a sentence with because because because is a conjunction";
+console.log(first.indexOf("because"));
+
+//18
+var occurrence = "You cannot end a sentence with because because because is a conjunction";
+console.log(occurrence.lastIndexOf("because"));
+
+//19
+var searchName = "You cannot end a sentence with because because because is a conjunction";
+console.log(searchName.search("because"));
+
+//20
+var space = " You cannot end a sentence with because because because is a conjunction ";
+console.log(space);
+console.log(space.trim(" ")); //only start and end space remove
+
+//21
+var start = 'learn Javascript';
+console.log(start.startsWith("lea"));//true
+console.log(start.startsWith("java"));//false
+
+//22
+var end = 'learn Javascript';
+console.log(end.endsWith("lea"));//false
+console.log(end.endsWith("Javascript"));//true
+
+//23
+let mathod1 = "learn javascript"
+console.log(mathod1.match("a"));
+
+//24
+let merge1 = 'learn javascript'
+console.log(merge1.concat(" 30 Days Of", " JavaScript"));
+
+//25
+let repeat1= 'learn javascript'
+console.log(repeat1.repeat(2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
